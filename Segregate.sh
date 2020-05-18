a87edfd05bfe5b3d6304db00e8a227344fbac7a2
Eswar Abisheak <47601182+eswar2001@users.noreply.github.com>
2020-05-18 18:16:22 +0530

    Add files via upload

---
+++ Segregate.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+for i in *; do
+    dir=$(file "$i" | awk -F ': |, ' '{print $2}');
+    if [[ "$i" != "directory" ]]; then
+        mkdir -p "${dir%% *}"
+        mv "$i" "${dir%% *}/$i"
+    fi
+done
